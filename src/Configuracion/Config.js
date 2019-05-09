448ffe30250cd1260dd5a4959de37eab4ce8a30d
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-05-09 18:11:59 -0500

    winterfell commits

--- src/Configuracion/Config.js
+++
@@ -1,4 +0,0 @@
-
-   const CONFIG= 'https://sigadev1-consultarecibos-back.herokuapp.com/'   
-   //const CONFIG= 'http://localhost:8080/';
-export default CONFIG;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Componentes/App';
-//import AppCodigo from './Componentes/AppCodigo';
-import AppNueva from './Componentes/AppNueva';
-import AppNueva2 from './Componentes/AppNueva2';
-import LoginFormNombreApellidos from './Componentes/LoginFormNombreApellidos';
+import { BrowserRouter as Router } from "react-router-dom";
+import App from './Component/App';
 import registerServiceWorker from './registerServiceWorker';
-import { Router, Route, browserHistory } from 'react-router-3';
-import LoginForm from './Componentes/LoginForm';
-import VistaTablaNuevo from './Componentes/VistaTablaNueva';
-import VistaIntermedia from './Componentes/seleccion-intermedia';
-import ComponenteEditable from './Componentes/ComponenteEditable';
-import Formulario from './Componentes/formulario';
 
-class Index extends React.Component {
-    render() {
-        return(
-            <Router history={browserHistory}>
-            <Route
-                component={() => <LoginForm />}
-                path="/">
-            </Route>
-            
-            <Route path="/:name" component={App}></Route>
-            <Route path="/filtro/:name" component={VistaIntermedia}></Route>
-            <Route path="/vista/nueva" component={AppNueva}></Route>
-            <Route path="/vista/nueva2" component={AppNueva2}></Route>
-            <Route path="/vista/tabla" component={ VistaTablaNuevo}></Route>
-            <Route path="/vista/loginNyA" component={LoginFormNombreApellidos}></Route>
-            <Route path="/vista/imprimir" component={ComponenteEditable}></Route>
-            <Route path="/formulario/:codigo" component={Formulario}></Route>
-            
-          </Router>
-          )
-      }
-      
-}
-
-ReactDOM.render(
-    <Index/>, document.getElementById('root'));
-
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
